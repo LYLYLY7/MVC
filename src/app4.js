94473552fe6015372c19a429905f966a8d427bdf
LYLYLY <lylyly72020@163.com>
2022-12-25 10:29:21 +0800

    app4 的html封装到app4.js中

--- src/app4.js
+++ src/app4.js
@@ -1,9 +1,16 @@
 import $ from 'jquery';
 import './app4.css';
 
-const $circle  = $('#app4 .circle')
-$circle.on('mouseenter',()=>{
-  $circle.addClass('active')
-}).on('mouseleave',()=>{
-  $circle.removeClass('active')
-})
+const html = `
+    <section id="app4">
+      <div class="circle"></div>
+    </section>
+`;
+const $element = $(html).appendTo($(`body > .page`));
+
+const $circle = $('#app4 .circle');
+$circle.on('mouseenter', () => {
+  $circle.addClass('active');
+}).on('mouseleave', () => {
+  $circle.removeClass('active');
+});
