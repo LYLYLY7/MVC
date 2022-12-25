3bcd546b19b0897689e1d5fb1f7652b08abbbb6f
LYLYLY <lylyly72020@163.com>
2022-12-25 09:51:34 +0800

    为app2 添加刷新依旧是当前标签的功能；localStorage(getItem,setItem)

--- src/app2.js
+++ src/app2.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 
 const $tabBar = $('#app2 .tab-bar');
 const $tabContent = $('#app2 .tab-content');
+const index =localStorage.getItem('app2.index') || 0
 $tabBar.on('click', 'li', (e) => {
   // 获取子元素
   const $li = $(e.currentTarget);
@@ -12,6 +13,7 @@ $tabBar.on('click', 'li', (e) => {
     .removeClass('selected');
   // 获取子元素在兄弟之间排行老几
   const index = $li.index();
+  localStorage.setItem('app2.index',index)
   // 找到孩子 ，然后将index的孩子css变为
   $tabContent.children()
     .eq(index).addClass('active')
@@ -19,4 +21,4 @@ $tabBar.on('click', 'li', (e) => {
   // eq是等于的意思
 });
 // 找到111和222里面的索引为0的元素，触发click事件
-$tabBar.children().eq(0).trigger('click')
+$tabBar.children().eq(index).trigger('click')
